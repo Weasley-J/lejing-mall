2cdf41fec08b064054158ce522f8f48a0d4c62c8
Weasley <1432689025@qq.com>
2021-02-01 01:41:01 +0800

    解决nacos引入依赖后导致springboot项目启动失败

--- lejing-coupon/src/main/java/cn/alphahub/mall/coupon/LejingCouponApplication.java
+++ lejing-coupon/src/main/java/cn/alphahub/mall/coupon/LejingCouponApplication.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScans(value = {@MapperScan(value = {"cn.**.dao", "cn.**.mapper"})})
 public class LejingCouponApplication {
