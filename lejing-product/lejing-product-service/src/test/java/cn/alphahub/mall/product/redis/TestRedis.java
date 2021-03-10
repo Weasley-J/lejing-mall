7ae16616b1263e3c1f8c5f283bfffca77f897676
Weasley <1432689025@qq.com>
2021-03-11 02:05:09 +0800

    update

--- lejing-product/lejing-product-service/src/test/java/cn/alphahub/mall/product/redis/TestRedis.java
+++ lejing-product/lejing-product-service/src/test/java/cn/alphahub/mall/product/redis/TestRedis.java
@@ -1,6 +1,5 @@
 package cn.alphahub.mall.product.redis;
 
-import cn.hutool.json.JSONUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
