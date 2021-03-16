0fe4fd873db6d3b0630e0a59bdb672311e0caf0d
Weasley <1432689025@qq.com>
2021-03-17 03:37:12 +0800

    add authorization service

---
+++ lejing-auth/lejing-auth-service/src/main/java/cn/alphahub/mall/auth/LejingAuthApplication.java
@@ -0,0 +1,23 @@
+package cn.alphahub.mall.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 乐璟商城-授权服务
+ *
+ * @author liuwenjing
+ * @date 2021年3月17日
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class LejingAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LejingAuthApplication.class, args);
+    }
+
+}
---
+++ lejing-auth/lejing-auth-service/src/test/java/cn/alphahub/mall/auth/LejingAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.alphahub.mall.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LejingAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
