049e2b94e063a4749015733f73a455054993d49c
Weasley <1432689025@qq.com>
2021-03-28 23:54:00 +0800

    update

--- lejing-auth/lejing-demo-sso-server/src/main/java/cn/alphahub/mall/auth/sso/server/controller/LoginController.java
+++ lejing-auth/lejing-demo-sso-server/src/main/java/cn/alphahub/mall/auth/sso/server/controller/LoginController.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class LoginController {
 
     @Resource
-    StringRedisTemplate stringRedisTemplate;
+    public StringRedisTemplate stringRedisTemplate;
 
     @ResponseBody
     @GetMapping("/userinfo")
