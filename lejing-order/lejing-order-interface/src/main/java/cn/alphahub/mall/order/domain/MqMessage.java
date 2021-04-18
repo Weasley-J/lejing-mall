979dea5de2fc19db09364a33913047f4d8e4ca46
Weasley <1432689025@qq.com>
2021-04-18 16:35:38 +0800

    update

--- lejing-order/lejing-order-interface/src/main/java/cn/alphahub/mall/order/domain/MqMessage.java
+++ lejing-order/lejing-order-interface/src/main/java/cn/alphahub/mall/order/domain/MqMessage.java
@@ -52,7 +52,7 @@ public class MqMessage implements Serializable {
     private String classType;
 
     /**
-     * 0-新建1-已发送2错误抵达3-已抵达
+     * 0-新建，1-已发送，2-错误抵达，3-已抵达
      */
     private Integer messageStatus;
 
