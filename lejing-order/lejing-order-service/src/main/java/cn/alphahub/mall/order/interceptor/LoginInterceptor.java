5e7cdb35c272f6bc673c2b9b41f908cf0e0f270c
Weasley <1432689025@qq.com>
2021-04-19 23:25:42 +0800

    update

---
+++ lejing-order/lejing-order-service/src/main/java/cn/alphahub/mall/order/interceptor/LoginInterceptor.java
@@ -0,0 +1,34 @@
+package cn.alphahub.mall.order.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <b>登录拦截器</b>
+ *
+ * @author liuwenjing
+ * @version 1.0
+ * @date 2021/04/19
+ */
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
