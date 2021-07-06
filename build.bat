9c81ad7c6177c0bf83f5564761e21a1284c65155
Wenjing Liu <1432689025@qq.com>
2021-07-06 14:54:27 +0800

    update

--- build.bat
+++ build.bat
@@ -1,5 +1,33 @@
 @echo off & color 0A
-setlocal enabledelayedexpansion
-set name=���springbootӦ��Ϊ��ִ��jar�ļ�
-title %name%                 ����: %date%    ʱ��: %time%
-cls && mvn clean && mvn package && exit
+
+REM 只打包某个service
+REM mvn clean package -pl "lejing-auth/lejing-auth-service" -am ^
+
+REM 安装基础依赖到本地仓库, 方便后续service打包
+mvn clean package install -pl "lejing-common/lejing-common-util" -am ^
+&& mvn clean package install -pl "lejing-common/lejing-common-base-domain" -am ^
+&& mvn clean package install -pl "lejing-common/lejing-common-base-public" -am ^
+&& mvn clean package install -pl "lejing-common/lejing-common-dependencies-db" -am ^
+&& mvn clean package install -pl "lejing-common/lejing-common-dependencies-web" -am ^
+
+&& mvn clean package install -pl "lejing-auth/lejing-auth-common" -am ^
+&& mvn clean package install -pl "lejing-auth/lejing-auth-interface" -am ^
+
+&& mvn clean package install -pl "lejing-cart/lejing-cart-interface" -am ^
+
+&& mvn clean package install -pl "lejing-coupon/lejing-coupon-interface" -am ^
+
+&& mvn clean package install -pl "lejing-member/lejing-member-interface" -am ^
+
+&& mvn clean package install -pl "lejing-order/lejing-order-interface" -am ^
+
+&& mvn clean package install -pl "lejing-product/lejing-product-interface" -am ^
+
+&& mvn clean package install -pl "lejing-search/lejing-search-interface" -am ^
+
+&& mvn clean package install -pl "lejing-seckill/lejing-seckill-interface" -am ^
+
+&& mvn clean package install -pl "lejing-ware/lejing-ware-interface" -am ^
+
+&& cls && mvn clean ^
+&& exit
--- build.sh
+++ build.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-mvn clean && mvn package && mvn clean
+# 只打包某个service
+# mvn clean package -pl "lejing-auth/lejing-auth-service" -am
+
+# 安装基础依赖到本地仓库, 方便后续service打包
+mvn clean package install -pl "lejing-common/lejing-common-util" -am \
+&& mvn clean package install -pl "lejing-common/lejing-common-base-domain" -am \
+&& mvn clean package install -pl "lejing-common/lejing-common-base-public" -am \
+&& mvn clean package install -pl "lejing-common/lejing-common-dependencies-db" -am \
+&& mvn clean package install -pl "lejing-common/lejing-common-dependencies-web" -am \
+&& mvn clean package install -pl "lejing-auth/lejing-auth-common" -am \
+&& mvn clean package install -pl "lejing-auth/lejing-auth-interface" -am \
+&& mvn clean package install -pl "lejing-cart/lejing-cart-interface" -am \
+&& mvn clean package install -pl "lejing-coupon/lejing-coupon-interface" -am \
+&& mvn clean package install -pl "lejing-member/lejing-member-interface" -am \
+&& mvn clean package install -pl "lejing-order/lejing-order-interface" -am \
+&& mvn clean package install -pl "lejing-product/lejing-product-interface" -am \
+&& mvn clean package install -pl "lejing-search/lejing-search-interface" -am \
+&& mvn clean package install -pl "lejing-seckill/lejing-seckill-interface" -am \
+&& mvn clean package install -pl "lejing-ware/lejing-ware-interface" -am \
+&& clear
+
