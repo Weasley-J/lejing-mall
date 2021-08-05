d1c1a4500082c6c252b9abf22b5c772b3910200d
liuwenjing <1432689025@qq.com>
2021-08-05 22:11:40 +0800

    update

--- lejing-common/lejing-common-base-public/src/main/java/cn/alphahub/common/exception/GlobalExceptionHandler.java
+++ lejing-common/lejing-common-base-public/src/main/java/cn/alphahub/common/exception/GlobalExceptionHandler.java
@@ -1,8 +1,7 @@
 package cn.alphahub.common.exception;
 
 import cn.alphahub.common.core.domain.BaseResult;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -13,9 +12,9 @@ import org.yaml.snakeyaml.constructor.DuplicateKeyException;
  *
  * @author liuwenjing
  */
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    private final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     /**
      * 处理自定义异常
