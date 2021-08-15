9f773008fb0e7eb42371cfe2ff3f940a1cca9870
Weasley J <1432689025@qq.com>
2021-08-15 19:47:17 +0800

    update build script.

--- build.bat
+++ build.bat
@@ -1,36 +1,36 @@
 @echo off & color 0A
 
 REM 只打包某个service
-REM mvn clean package -pl "lejing-auth/lejing-auth-service" -am ^
+REM mvn clean package -pl :lejing-auth-service -am ^
 
 REM 安装基础依赖到本地仓库, 方便后续service打包
 
 mvn clean ^
-&& mvn clean install -pl "lejing-common/lejing-common-util" -am ^
-&& mvn clean install -pl "lejing-common/lejing-common-base-domain" -am ^
-&& mvn clean install -pl "lejing-common/lejing-common-base-public" -am ^
-&& mvn clean install -pl "lejing-common/lejing-common-dependencies-db" -am ^
-&& mvn clean install -pl "lejing-common/lejing-common-dependencies-web" -am ^
+&& mvn clean install -pl :lejing-common-util -am ^
+&& mvn clean install -pl :lejing-common-base-domain -am ^
+&& mvn clean install -pl :lejing-common-base-public -am ^
+&& mvn clean install -pl :lejing-common-dependencies-db -am ^
+&& mvn clean install -pl :lejing-common-dependencies-web -am ^
 
-&& mvn clean install -pl "lejing-auth/lejing-auth-common" -am ^
-&& mvn clean install -pl "lejing-auth/lejing-auth-interface" -am ^
+&& mvn clean install -pl :lejing-auth-common -am ^
+&& mvn clean install -pl :lejing-auth-interface -am ^
 
-&& mvn clean install -pl "lejing-cart/lejing-cart-interface" -am ^
+&& mvn clean install -pl :lejing-cart-interface -am ^
 
-&& mvn clean install -pl "lejing-coupon/lejing-coupon-interface" -am ^
+&& mvn clean install -pl :lejing-coupon-interface -am ^
 
-&& mvn clean install -pl "lejing-member/lejing-member-interface" -am ^
+&& mvn clean install -pl :lejing-member-interface -am ^
 
-&& mvn clean install -pl "lejing-order/lejing-order-interface" -am ^
-&& mvn clean install -pl "lejing-order/lejing-order-share" -am ^
+&& mvn clean install -pl :lejing-order-interface -am ^
+&& mvn clean install -pl :lejing-order-share -am ^
 
-&& mvn clean install -pl "lejing-product/lejing-product-interface" -am ^
+&& mvn clean install -pl :lejing-product-interface -am ^
 
-&& mvn clean install -pl "lejing-search/lejing-search-interface" -am ^
+&& mvn clean install -pl :lejing-search-interface -am ^
 
-&& mvn clean install -pl "lejing-seckill/lejing-seckill-interface" -am ^
+&& mvn clean install -pl :lejing-seckill-interface -am ^
 
-&& mvn clean install -pl "lejing-ware/lejing-ware-interface" -am ^
+&& mvn clean install -pl :lejing-ware-interface -am ^
 
 && cls && mvn clean ^
 && exit
--- build.sh
+++ build.sh
@@ -5,20 +5,20 @@
 # 安装基础依赖到本地仓库, 方便后续service打包
 
 mvn clean &&
-  mvn clean install -pl "lejing-common/lejing-common-util" -am &&
-  mvn clean install -pl "lejing-common/lejing-common-base-domain" -am &&
-  mvn clean install -pl "lejing-common/lejing-common-base-public" -am &&
-  mvn clean install -pl "lejing-common/lejing-common-dependencies-db" -am &&
-  mvn clean install -pl "lejing-common/lejing-common-dependencies-web" -am &&
-  mvn clean install -pl "lejing-auth/lejing-auth-common" -am &&
-  mvn clean install -pl "lejing-auth/lejing-auth-interface" -am &&
-  mvn clean install -pl "lejing-cart/lejing-cart-interface" -am &&
-  mvn clean install -pl "lejing-coupon/lejing-coupon-interface" -am &&
-  mvn clean install -pl "lejing-member/lejing-member-interface" -am &&
-  mvn clean install -pl "lejing-order/lejing-order-interface" -am &&
-  mvn clean install -pl "lejing-order/lejing-order-share" -am &&
-  mvn clean install -pl "lejing-product/lejing-product-interface" -am &&
-  mvn clean install -pl "lejing-search/lejing-search-interface" -am &&
-  mvn clean install -pl "lejing-seckill/lejing-seckill-interface" -am &&
-  mvn clean install -pl "lejing-ware/lejing-ware-interface" -am &&
+  mvn clean install -pl :lejing-common-util -am &&
+  mvn clean install -pl :lejing-common-base-domain -am &&
+  mvn clean install -pl :lejing-common-base-public -am &&
+  mvn clean install -pl :lejing-common-dependencies-db -am &&
+  mvn clean install -pl :lejing-common-dependencies-web -am &&
+  mvn clean install -pl :lejing-auth-common -am &&
+  mvn clean install -pl :lejing-auth-interface -am &&
+  mvn clean install -pl :lejing-cart-interface -am &&
+  mvn clean install -pl :lejing-coupon-interface -am &&
+  mvn clean install -pl :lejing-member-interface -am &&
+  mvn clean install -pl :lejing-order-interface -am &&
+  mvn clean install -pl :lejing-order-share -am &&
+  mvn clean install -pl :lejing-product-interface -am &&
+  mvn clean install -pl :lejing-search-interface -am &&
+  mvn clean install -pl :lejing-seckill-interface -am &&
+  mvn clean install -pl :lejing-ware-interface -am &&
   mvn clean && clear
