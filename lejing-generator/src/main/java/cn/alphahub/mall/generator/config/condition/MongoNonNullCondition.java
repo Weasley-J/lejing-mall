a1586fc52d3d30295e7e1762079195b18e2cba27
Weasley <1432689025@qq.com>
2021-08-27 10:25:23 +0800

    update

--- lejing-generator/src/main/java/cn/alphahub/mall/generator/config/condition/MongoNonNullCondition.java
+++ lejing-generator/src/main/java/cn/alphahub/mall/generator/config/condition/MongoNonNullCondition.java
@@ -2,6 +2,7 @@ package cn.alphahub.mall.generator.config.condition;
 
 import cn.alphahub.mall.generator.enums.DbType;
 import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
@@ -16,6 +17,6 @@ public class MongoNonNullCondition implements Condition {
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
         String property = context.getEnvironment().getProperty("code.generator.db-type", String.class);
-        return DbType.MONGODB.getCode().equalsIgnoreCase(property);
+        return StringUtils.equalsIgnoreCase(DbType.MONGODB.getCode(), property);
     }
 }
