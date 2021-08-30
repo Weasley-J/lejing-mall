b799fe910dc55c92c22df95845e6a5ba26950dda
Weasley J <1432689025@qq.com>
2021-08-30 19:58:18 +0800

    update

--- lejing-job/src/main/java/cn/alphahub/mall/schedule/job/module/seckill/ScheduleOnShelveSeckillSkuLatest3Days.java
+++ lejing-job/src/main/java/cn/alphahub/mall/schedule/job/module/seckill/ScheduleOnShelveSeckillSkuLatest3Days.java
@@ -2,11 +2,13 @@ package cn.alphahub.mall.schedule.job.module.seckill;
 
 import cn.alphahub.common.core.domain.BaseResult;
 import cn.alphahub.mall.schedule.feign.seckill.SeckillClient;
-import cn.hutool.extra.spring.SpringUtil;
 import cn.hutool.json.JSONUtil;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * Job - 上架最近三天的秒杀商品
@@ -21,11 +23,14 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
  * @version 1.0
  * @date 2021-08-30 19:47
  */
+@Component
 public class ScheduleOnShelveSeckillSkuLatest3Days extends QuartzJobBean {
+
+    @Resource
+    private SeckillClient seckillClient;
 
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        SeckillClient seckillClient = SpringUtil.getBean(SeckillClient.class);
         BaseResult<Void> result = seckillClient.onShelveSeckillSkuLatest3Days();
         System.err.println("任务调度上架最近三天的秒杀商品:'" + JSONUtil.toJsonStr(result) + "'");
     }
