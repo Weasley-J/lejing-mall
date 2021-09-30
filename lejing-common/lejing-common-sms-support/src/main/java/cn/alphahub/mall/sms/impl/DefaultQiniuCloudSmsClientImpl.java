577b166e3290a7c4746baadfbbc38c801f498007
Weasley J <1432689025@qq.com>
2021-09-30 17:24:53 +0800

    完善多短信模板、多短信供应商的maven版本控制

--- lejing-common/lejing-common-sms-support/src/main/java/cn/alphahub/mall/sms/impl/DefaultQiniuCloudSmsClientImpl.java
+++ lejing-common/lejing-common-sms-support/src/main/java/cn/alphahub/mall/sms/impl/DefaultQiniuCloudSmsClientImpl.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static cn.alphahub.mall.sms.config.SmsConfig.SmsProperties;
@@ -83,7 +84,10 @@ public class DefaultQiniuCloudSmsClientImpl implements SmsClient {
             */
 
         } catch (QiniuException e) {
-            log.error("error:{}", e.getLocalizedMessage(), e);
+            log.error("{}", e.getLocalizedMessage(), e);
+            Map<String, Object> responseMap = new LinkedHashMap<>(1);
+            responseMap.put("error", e.getLocalizedMessage());
+            return JSONUtil.toJsonStr(responseMap);
         }
         return resp;
     }
