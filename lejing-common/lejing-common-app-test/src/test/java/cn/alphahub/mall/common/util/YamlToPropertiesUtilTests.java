c2219d1938eb14535da326bae58940221d427a11
liuwenjing <liuwenjing@fosunhealth.com>
2022-07-02 20:30:06 +0800

    update

--- lejing-common/lejing-common-app-test/src/test/java/cn/alphahub/mall/common/util/YamlToPropertiesUtilTests.java
+++ lejing-common/lejing-common-app-test/src/test/java/cn/alphahub/mall/common/util/YamlToPropertiesUtilTests.java
@@ -3,7 +3,9 @@ package cn.alphahub.mall.common.util;
 import cn.alphahub.common.util.YamlToPropertiesUtil;
 import cn.hutool.json.JSONUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.InputStream;
 import java.util.Properties;
@@ -13,11 +15,18 @@ public class YamlToPropertiesUtilTests {
 
     @Test
     void yml2prop() {
-        InputStream inputStream = Thread
-                .currentThread()
+        InputStream inputStream = Thread.currentThread()
                 .getContextClassLoader()
                 .getResourceAsStream("application.yml");
         Properties properties = YamlToPropertiesUtil.toProperties(inputStream);
+        System.err.println(JSONUtil.toJsonPrettyStr(properties));
+    }
+
+    @Test
+    void yml2propTwo() {
+        YamlPropertiesFactoryBean yamlMapFactoryBean = new YamlPropertiesFactoryBean();
+        yamlMapFactoryBean.setResources(new ClassPathResource("application.yml"), new ClassPathResource("static/application-dev.yml"));
+        Properties properties = yamlMapFactoryBean.getObject();
         System.err.println(JSONUtil.toJsonPrettyStr(properties));
     }
 
