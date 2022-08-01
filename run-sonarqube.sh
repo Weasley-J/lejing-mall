af156e878652da3a9a48942824ebfa4e5a32d893
Weasley <1432689025@qq.com>
2022-08-01 21:59:59 +0800

    update

--- run-sonarqube.sh
+++ run-sonarqube.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SETTINGS="/Users/weasley/Development/program/apache-maven/conf/settings-sonarqube.xml"
-MODULE="lejing-mall"
+
 clear &&
-  mvn clean compile sonar:sonar -pl :$MODULE -am --settings $SETTINGS &&
+  mvn clean compile sonar:sonar --settings $SETTINGS &&
   mvn clean
