87578f8f4f5d727cdee17cd61f000531a5016ebc
Weasley <1432689025@qq.com>
2022-10-11 22:11:40 +0800

    update

--- lejing-coupon/lejing-coupon-service/src/main/java/cn/alphahub/mall/coupon/config/Jackson2ObjectMapperConfig.java
+++ lejing-coupon/lejing-coupon-service/src/main/java/cn/alphahub/mall/coupon/config/Jackson2ObjectMapperConfig.java
@@ -1,13 +1,19 @@
-package cn.alphahub.mall.seckill.config;
+package cn.alphahub.mall.coupon.config;
 
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.TimeZone;
 
@@ -17,7 +23,7 @@ import java.util.TimeZone;
  * @author liuwenjing
  */
 @Configuration
-public class LocalDateTimeSerializerConfig {
+public class Jackson2ObjectMapperConfig {
     /**
      * 日期时间格式,没有在yml里面配置默认采用: yyyy-MM-dd HH:mm:ss
      */
@@ -30,29 +36,19 @@ public class LocalDateTimeSerializerConfig {
     private String timeZone;
 
     /**
-     * @return LocalDateTime 序列化器
-     */
-    @Bean
-    public LocalDateTimeSerializer localDateTimeSerializer() {
-        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
-    }
-
-    /**
-     * @return LocalDateTime 反序列化器
-     */
-    @Bean
-    public LocalDateTimeDeserializer localDateTimeDeserializer() {
-        return new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(pattern));
-    }
-
-    /**
      * @return Jackson2ObjectMapperBuilderCustomizer
      */
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
         return builder -> {
-            builder.serializerByType(LocalDateTime.class, localDateTimeSerializer());
-            builder.deserializerByType(LocalDateTime.class, localDateTimeDeserializer());
+            builder.serializerByType(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
+            builder.serializerByType(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+            builder.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern)));
+
+            builder.deserializerByType(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
+            builder.deserializerByType(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+            builder.deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(pattern)));
+
             builder.simpleDateFormat(pattern);
             builder.timeZone(TimeZone.getTimeZone(timeZone));
         };
